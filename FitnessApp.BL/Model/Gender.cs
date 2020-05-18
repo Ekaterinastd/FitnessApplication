893091fae40592276d832a07d690c7cf09b576cc
Ekaterinastd <ekaterina.g2015@mail.ru>
2020-05-18 16:00:52 +0400

    Создана модель Пользователя

---
+++ FitnessApp.BL/Model/Gender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FitnessApp.BL.Model
+{
+    public class Gender
+    {
+        /// <summary>
+        /// Имя пола.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name">Имя пола</param>
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null ", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ FitnessApp.CMD/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessApp.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
