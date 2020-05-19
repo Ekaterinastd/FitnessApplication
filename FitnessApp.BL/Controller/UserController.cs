a0dce42397f14e515c71d9c0bfea0f3e010354b2
Ekaterinastd <ekaterina.g2015@mail.ru>
2020-05-20 01:20:54 +0400

    добавление теста для провекти сохранения

--- FitnessApp.BL/Controller/UserController.cs
+++ FitnessApp.BL/Controller/UserController.cs
@@ -52,8 +52,7 @@ namespace FitnessApp.BL.Controller
 
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-
-                if (formatter.Deserialize(fs) is List<User> users)
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<User> users)
                 {
                     return users;
                 }
@@ -77,7 +76,7 @@ namespace FitnessApp.BL.Controller
             }
         }
 
-        public void SetNewUserData(string genderName, DateTime birthDate, double weight=1, double height=1)
+        public void SetNewUserData(string genderName, DateTime birthDate, double weight = 1, double height = 1)
         {
             CurrentUser.Gender = new Gender(genderName);
             CurrentUser.BirthDate = birthDate;
---
+++ FitnessApp.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FitnessApp.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SaveTest()
+        {
+            //Arrage
+            var userName = Guid.NewGuid().ToString();
+            //Act
+            var controller = new UserController(userName);
+            //Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
