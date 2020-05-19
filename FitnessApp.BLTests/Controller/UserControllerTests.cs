bf535e63525102104b6acf3afa6bcf170ca85af5
Ekaterinastd <ekaterina.g2015@mail.ru>
2020-05-20 01:46:48 +0400

    Добавлено новое тестирование для контроллера пользователя.

--- FitnessApp.BLTests/Controller/UserControllerTests.cs
+++ FitnessApp.BLTests/Controller/UserControllerTests.cs
@@ -20,7 +20,19 @@ namespace FitnessApp.BL.Controller.Tests
         [TestMethod()]
         public void SetNewUserDataTest()
         {
-            Assert.Fail();
+            var userName = Guid.NewGuid().ToString();
+            var birthDate = DateTime.Now.AddDays(-18);
+            var weight = 90;
+            var height = 180;
+            var gender = "m";
+            var controller = new UserController(userName);
+            controller.SetNewUserData(gender, birthDate, weight, height);
+            var controller2 = new UserController(userName);
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+            Assert.AreEqual(gender, controller2.CurrentUser.Gender.Name);
+            Assert.AreEqual(birthDate, controller2.CurrentUser.BirthDate);
+            Assert.AreEqual(weight, controller2.CurrentUser.Weight);
+            Assert.AreEqual(height, controller2.CurrentUser.Height);
         }
     }
 }
